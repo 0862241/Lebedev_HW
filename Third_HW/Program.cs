500cff8a9eec0eab2804211d920b8c5f0e3304ed
0862241 <ptical@mail.ru>
2023-01-10 09:42:19 +0300

    Second exsercise

--- Third_HW/Program.cs
+++ Third_HW/Program.cs
@@ -28,6 +28,8 @@ else
 /* Exsercise 2. Write a program that takes the coordinates 
 of two points as input and finds the distance between them in 3D space.*/
 
+/*
+
 double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2 , 2) + Math.Pow(z1 - z2, 2));
@@ -48,3 +50,9 @@ Console.Write("Please enter z2 coordinate ");
 double z2 = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine(Distance(x1, y1, z1, x2, y2, z2));
+*/
+
+/* Exsercise 3. Write a program that takes a number (N) as input
+ and outputs a table of cubes of numbers from 1 to N. */
+
+ 
